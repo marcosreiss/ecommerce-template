37225300fd7cd3ceca062eb1c1299ecbf842ff51
Marcos Vinicius Reis <m.vinicius.sr11@gmail.com>
2025-09-19 16:37:33 -0300

    fix dockerfile frontend v2

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,10 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  output: "standalone", // 🔹 necessário para gerar .next/standalone
 };
 
 export default nextConfig;
